28841d3476211aef5feb32fd42b0a898616c2c36
jrbigmon <juniorterriaga0@gmail.com>
2023-10-01 18:09:31 -0300

    feat: adjusted create product to add id and timestamps

--- src/routes/products/gateways/product-gateway.memory.ts
+++ src/routes/products/gateways/product-gateway.memory.ts
@@ -2,11 +2,18 @@ import { Product } from '../entities/products.entity';
 import { ProductGatewayInterface } from './product-gateway.interface';
 import { Filter } from '../../../interfaces/filters.interface';
 import { ID_MUST_BE_PROVIDED, PRODUCT_NOT_FOUND } from '../utils/constants';
+import { randomUUID } from 'crypto';
 
 export class ProductGatewayMemory implements ProductGatewayInterface {
   private products: Product[] = [];
 
-  async create(value?: Product): Promise<Product> {
+  async create(value: Product): Promise<Product> {
+    const instantDate = new Date();
+
+    value.id = randomUUID();
+    value.createdAt = instantDate;
+    value.updateAt = instantDate;
+
     this.products.push(value);
 
     return value;
@@ -48,15 +55,17 @@ export class ProductGatewayMemory implements ProductGatewayInterface {
         .map((p) => p.price)
         .reduce((total, p = 0) => total + p);
     }
+
+    return 0;
   }
 
-  async findAll(filters: Filter<Product>): Promise<Product[]> {
+  async findAll(filters?: Filter<Product>): Promise<Product[]> {
     if (filters && Object.keys(filters)?.length) {
       return this.products.filter((p) => {
         let isMatch = true;
 
         for (const key in filters) {
-          if (!String(p[key]).includes(filters[key])) isMatch = false;
+          if (!String(p[key])?.includes(filters[key])) isMatch = false;
         }
 
         return isMatch;
