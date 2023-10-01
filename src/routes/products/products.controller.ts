cd18acc21e6e82f4defe7a41bbac8a66f9e81f40
jrbigmon <juniorterriaga0@gmail.com>
2023-10-01 18:07:30 -0300

    feat: created service and controller to products

--- src/routes/products/products.controller.ts
+++ src/routes/products/products.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ProductService } from './products.service';
+import { Product } from './entities/products.entity';
+import { ProductCreateDto } from './dto/product-create.dto';
 
-@Controller()
+@Controller('products')
 export class ProductController {
   constructor(private readonly service: ProductService) {}
+
+  @Post()
+  async create(@Body() body: ProductCreateDto): Promise<Product> {
+    try {
+      return await this.service.create(body);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
--- src/routes/products/products.service.ts
+++ src/routes/products/products.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { ProductGatewayInterface } from './gateways/product-gateway.interface';
+import { Product } from './entities/products.entity';
+import { ProductCreateDto } from './dto/product-create.dto';
+import { Filter } from '../../interfaces/filters.interface';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly productGateway: ProductGatewayInterface) {}
+
+  public async create(product: ProductCreateDto): Promise<Product> {
+    const productToCreate = new Product(
+      product?.name,
+      product?.price,
+      product?.description,
+      product?.imgURL,
+      product?.expired,
+    );
+
+    if (!productToCreate?.name) {
+      throw new Error('Name must be provided');
+    }
+
+    if (!productToCreate?.price) {
+      throw new Error('Price must be provided');
+    }
+
+    return await this.productGateway.create(productToCreate);
+  }
+
+  public async findAll(
+    filters?: Filter<Product>,
+    expired?: boolean,
+  ): Promise<Product[]> {
+    const resultsInDb = await this.productGateway.findAll(filters);
+
+    const products = resultsInDb?.map(
+      (r) =>
+        new Product(
+          r.name,
+          r.price,
+          r.description,
+          r.imgURL,
+          r.expired,
+          r.createdAt,
+          r.updateAt,
+          r.deletedAt,
+          r.id,
+        ),
+    );
+
+    if (!expired) {
+      return products.filter(
+        (product) => product.getExpirationDateInDays() > 0,
+      );
+    }
+
+    return products;
+  }
+
+  public async findById(id: string): Promise<Product> {
+    return await this.productGateway.findById(id);
+  }
+}
